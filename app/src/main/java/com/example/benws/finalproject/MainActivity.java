a7cbce781e8c5b3ee9fb9c6814cd8997f237ce04
Ben Sutter <bsutter2@illinois.edu>
2018-12-01 14:17:16 -0600

    All of the activities have been made and I am working to link them all together

--- app/src/main/java/com/example/benws/finalproject/MainActivity.java
+++ app/src/main/java/com/example/benws/finalproject/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.benws.finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button ulButton = findViewById(R.id.upperLeftButton);
+
+        ulButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Temp message to remind you this is Top Left", Toast.LENGTH_SHORT).show();
+                launchTL();
+            }
+        });
+    }
+
+    private void launchTL() {
+        Intent intent = new Intent(this, TopLeft_Activity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/benws/finalproject/TopLeft_Activity.java
@@ -0,0 +1,30 @@
+package com.example.benws.finalproject;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class TopLeft_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_left_);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "And we are headed back!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+    }
+
+}
