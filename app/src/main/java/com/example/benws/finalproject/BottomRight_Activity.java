a6f3b063baae67152355d62279160b37e79697be
Ben Sutter <bsutter2@illinois.edu>
2018-12-01 14:29:39 -0600

    All of the activities have been made and linked

--- app/src/main/java/com/example/benws/finalproject/BottomRight_Activity.java
+++ app/src/main/java/com/example/benws/finalproject/BottomRight_Activity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 public class BottomRight_Activity extends AppCompatActivity {
 
@@ -20,8 +21,8 @@ public class BottomRight_Activity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Toast.makeText(getApplicationContext(), "And we are headed back!", Toast.LENGTH_SHORT).show();
+                finish();
             }
         });
     }
--- app/src/main/java/com/example/benws/finalproject/MainActivity.java
+++ app/src/main/java/com/example/benws/finalproject/MainActivity.java
@@ -15,19 +15,61 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button ulButton = findViewById(R.id.upperLeftButton);
+        Button tlButton = findViewById(R.id.upperLeftButton);
 
-        ulButton.setOnClickListener(new View.OnClickListener() {
+        tlButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Temp message to remind you this is Top Left", Toast.LENGTH_SHORT).show();
                 launchTL();
             }
         });
+
+        Button trButton = findViewById(R.id.upperRightButton);
+
+        trButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Temp message to remind you this is Top Right", Toast.LENGTH_SHORT).show();
+                launchTR();
+            }
+        });
+
+        Button blButton = findViewById(R.id.bottomLeftButton);
+
+        blButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Temp message to remind you this is Bottom Left", Toast.LENGTH_SHORT).show();
+                launchBL();
+            }
+        });
+
+        Button brButton = findViewById(R.id.bottomRightButton);
+
+        brButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Temp message to remind you this is Bottom Right", Toast.LENGTH_SHORT).show();
+                launchBR();
+            }
+        });
     }
 
     private void launchTL() {
         Intent intent = new Intent(this, TopLeft_Activity.class);
+        startActivity(intent);
+    }
+    private void launchTR() {
+        Intent intent = new Intent(this, TopRight_Activity.class);
+        startActivity(intent);
+    }
+    private void launchBL() {
+        Intent intent = new Intent(this, BottomLeft_Activity.class);
+        startActivity(intent);
+    }
+    private void launchBR() {
+        Intent intent = new Intent(this, BottomRight_Activity.class);
         startActivity(intent);
     }
 }
