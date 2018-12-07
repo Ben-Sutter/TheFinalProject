d18d09bd14ac32ddd65691d8037245be5643bfdf
jmruth2 <jmruth2@illinois.edu>
2018-12-06 20:23:21 -0600

    participants

--- app/src/main/java/com/example/benws/finalproject/BottomLeftActivity.java
+++ app/src/main/java/com/example/benws/finalproject/BottomLeftActivity.java
@@ -3,6 +3,7 @@ package com.example.benws.finalproject;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -10,12 +11,33 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class BottomLeftActivity extends AppCompatActivity {
 
-    final int ID_SUBTRACT = 2131296385;
+    private static final String TAG = "FinalProject:TopLeft";
+
+    private static RequestQueue requestQueue;
+    private JSONObject input;
+
+    private RadioButton checkedRadioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        requestQueue = Volley.newRequestQueue(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_left);
 
@@ -42,11 +64,73 @@ public class BottomLeftActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 int selected = g.getCheckedRadioButtonId();
-                RadioButton checkedRadioButton = findViewById(selected);
+                checkedRadioButton = findViewById(selected);
                 int buttonChoice = Integer.parseInt(checkedRadioButton.getText().toString());
-                result.setText(String.valueOf(buttonChoice));
+                //result.setText(String.valueOf(buttonChoice));
+                setPartText(buttonChoice);
             }
         });
+    }
+
+    void setPartText(int numPeople) {
+        startAPICall(numPeople);
+    }
+
+    void setTheTextForReal(int numPeople) {
+        String randomPartName;
+        int randomPartPrice = 0;
+        int randomPartPeople = 0;
+        int randomPartAccess = 0;
+        try {
+            randomPartName = input.getString("activity");
+            randomPartPrice = 100 * input.getInt("price");
+            randomPartPeople = input.getInt("participants");
+            randomPartAccess = input.getInt("accessibility");
+        } catch (JSONException e) {
+            randomPartName = "ERROR";
+        }
+        String textInAll =
+                randomPartName + "\nPrice: $" + randomPartPrice
+                        + "\nParticipants: " + randomPartPeople
+                        + "\nAccessibility: " + randomPartAccess;
+        ((TextView) findViewById(R.id.partResults)).setText(textInAll);
+    }
+
+
+    void startAPICall(final int numPeople) {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "http://www.boredapi.com/api/activity?participants=" + numPeople,
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            input = response;
+                            //Toast.makeText(getApplicationContext(), "" + input, Toast.LENGTH_SHORT).show();
+                            Log.d(TAG, response.toString());
+                            setTheTextForReal(numPeople);
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.w(TAG, error.toString());
+                }
+            }
+            ) {
+                @Override
+                public Map<String, String> getHeaders() {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("Ocp-Apim-Subscription-Key", "YOUR_API_KEY");
+                    Log.d(TAG, params.toString());
+                    return params;
+                }
+            };
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //URL http://www.boredapi.com/api/activity
     }
 
     //Calling to the api
