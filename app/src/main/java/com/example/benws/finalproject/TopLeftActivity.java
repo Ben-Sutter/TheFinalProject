3f815d38f65d76f8c3ddd3abf966db5f80996eec
Ben Sutter <bsutter2@illinois.edu>
2018-12-08 16:52:26 -0600

    price now works

--- app/src/main/java/com/example/benws/finalproject/TopLeftActivity.java
+++ app/src/main/java/com/example/benws/finalproject/TopLeftActivity.java
@@ -8,17 +8,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -40,7 +37,7 @@ public class TopLeftActivity extends AppCompatActivity {
         setContentView(R.layout.activity_top_left);
 
         //This section of code is used to return to the main activity.
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -49,7 +46,7 @@ public class TopLeftActivity extends AppCompatActivity {
         });
 
         //Sets button functionality to call new random activity.
-        Button newRandom = (Button) findViewById(R.id.newRandomButton);
+        Button newRandom = findViewById(R.id.newRandomButton);
         newRandom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
